626284ea7530ae77f8f2b38d0860841cf7d98025
Drake Axelrod <drake@draxel.io>
2022-04-06 12:23:22 +0200

    Fix Commit Hook Script
    The curl link moved so had to update the script

---
+++ cyphy/src/TestCyphy.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this once per test-runner!
+
+#include "catch.hpp"
+
+TEST_CASE("Test Demo 1.") {
+    REQUIRE(0);
+}
---
+++ scripts/add-commit-hook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl https://cdn.rawgit.com/tommarshall/git-good-commit/v0.6.1/hook.sh > .git/hooks/commit-msg && chmod +x .git/hooks/commit-msg
