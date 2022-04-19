62b4d01ea3551d14d38eb2b78805697823e472da
Vernita <vernita@net.chalmers.se>
2022-04-19 11:21:29 +0200

    initial code for coverage implementation

---
+++ src/SensorReading.hpp
@@ -0,0 +1,11 @@
+#ifndef SENSOR_READING_HPP__
+#define SENSOR_READING_HPP__
+
+class SensorReading
+{
+public:
+   int Addition(int x, int y);
+   int Multiply(int x, int y);
+};
+
+#endif
--- src/Test.cpp
+++ src/Test.cpp
@@ -1,11 +1,14 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this once per test-runner!
 
 #include "catch.hpp"
+#include "SensorReading.hpp"
 
-TEST_CASE("Test Demo Pass.") {
-    REQUIRE(0);
+TEST_CASE("Test Addition") {
+    SensorReading Test;
+    REQUIRE(Test.Addition(4,5) == 9);
 }
 
-TEST_CASE("Test Demo Fail") {
-    REQUIRE(1);
+TEST_CASE("Test Multiplication") {
+    SensorReading Test;
+    REQUIRE(Test.Multiply(1,2) == 2);
 }
