1fd317a92bc224a1d4cd1625b5f87a061fe43a03
Vernita <vernita@net.chalmers.se>
2022-04-20 13:06:29 +0200

    initial changes for coverage

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# PATH_TO_BUILD_SCRIPT=/opt/sources/2022-group-05/bin/build.sh
+function print_stage() {
+    local sep="======================================================"
+}
+
+echo "THE SCRIPT HAS RUN"
+
+# working dir once started is /opt/sources
+
+# lcov --capture --directory build/CMakeFiles/Cyphy.dir --output-file coverage.info
+
+# install lcov
+# sudo apt install lcov
