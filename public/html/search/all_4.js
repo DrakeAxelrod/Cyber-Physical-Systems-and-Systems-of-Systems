f709fe300a455d33400fd57e4fba7550ad90cc1b
Drake Axelrod <drake@draxel.io>
2022-04-25 17:10:28 +0200

    Add document generation
    This is so we have something to show.

---
+++ public/html/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main_0',['main',['../main_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;main.cpp'],['../opencv__template_8cpp.html#af19ddca125f05a2090dd132045573ee4',1,'main(int32_t argc, char **argv):&#160;opencv_template.cpp']]],
+  ['main_2ecpp_1',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['multiply_2',['Multiply',['../classSensorReading.html#a7b352efe225dac022f10da38f04b3815',1,'SensorReading']]]
+];
---
+++ public/html/search/files_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sensorreading_2ecpp_0',['SensorReading.cpp',['../SensorReading_8cpp.html',1,'']]],
+  ['sensorreading_2ehpp_1',['SensorReading.hpp',['../SensorReading_8hpp.html',1,'']]]
+];
