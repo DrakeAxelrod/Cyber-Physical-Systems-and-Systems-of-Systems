af528087780acb6bcdf7e89588934820bf8bc0d0
Drake Axelrod <drake@draxel.io>
2022-05-02 20:27:30 +0200

    Add detection and visual display of cones
    Still need to figure out how to detemine steering. The visual aids
    Should help however.
    
    Issue #37
    
    Co-authored-by: @sicily
    Co-authored-by: @klarasve

--- src/opencv-template.cpp
+++ src/opencv-template.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2020  Christian Berger
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "steering-angle-generator.hpp"
 
 const std::string hsv_window_name = "HSVView";
@@ -23,8 +6,8 @@ std::vector<std::vector<cv::Point>> b_pts;
 std::vector<std::vector<cv::Point>> y_pts;
 
 // upper and lower bounds of the yellow and blue cones (hsv)
-Bound <cv::Scalar> yellow(cv::Scalar(17, 89, 128), cv::Scalar(35, 175, 216));
-Bound <cv::Scalar> blue(cv::Scalar(70, 43, 34), cv::Scalar(120, 255, 255));
+Bound<cv::Scalar> yellow(cv::Scalar(17, 89, 128), cv::Scalar(35, 175, 216));
+Bound<cv::Scalar> blue(cv::Scalar(70, 43, 34), cv::Scalar(120, 255, 255));
 
 HSVBounds hsv_bounds = HSVBounds(17, 35, 89, 175, 128, 216);
 // center point of feed
@@ -32,7 +15,8 @@ cv::Point center_pt;
 // booleans to trach whether a given cone has been detected
 bool b_detected = false;
 bool y_detected = false;
-
+float steering_angle = 0;
+float threshold = 50;
 struct Images
 {
   cv::Mat main;
@@ -44,14 +28,141 @@ struct Images
   cv::Mat hsv_debug;
 } Imgs;
 
-bool detect_blue(cv::Mat hsv_frame) {
+bool detectBlueCones(cv::Mat hsv_frame)
+{
   cv::findContours(hsv_frame, b_pts, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point());
   return b_pts.size() > 0 ? true : false;
 }
 
-bool detect_yellow(cv::Mat hsv_frame) {
+bool detectYellowCones(cv::Mat hsv_frame)
+{
   cv::findContours(hsv_frame, y_pts, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point());
   return y_pts.size() > 0 ? true : false;
+}
+
+cv::Rect getBlueBoundingBox(cv::Mat cropped_frame, float distance)
+{
+  // bgr (blue, green, red)
+  cv::Scalar blue(255, 0, 0);
+  cv::Rect previous_box(cv::Point(0, 0), cv::Size(0, 0));
+  // create a bounding box from the contour points of the cone and draw it
+  // only if the size of the bounding box is larger than a certain threshold
+  for (auto &contour : b_pts)
+  {
+    cv::Rect bounding_box = cv::boundingRect(contour);
+    if (bounding_box.area() > threshold)
+    {
+      cv::Rect box = cv::boundingRect(contour);
+      // draw the bounding box
+      cv::rectangle(cropped_frame, box, blue, 2);
+      // draw the center of the bounding box
+      cv::Point center(box.x + box.width / 2, box.y + box.height / 2);
+      cv::circle(cropped_frame, center, 5, blue, -1);
+      // draw the center of the previous bounding box
+      if (previous_box.width > 0)
+      {
+        cv::Point previous_center(previous_box.x + previous_box.width / 2, previous_box.y + previous_box.height / 2);
+        cv::circle(cropped_frame, previous_center, 5, blue, -1);
+        // draw a line between the center of the previous bounding box and the center of the current bounding box
+        cv::line(cropped_frame, previous_center, center, blue, 2);
+      }
+      previous_box = box;
+    }
+  }
+  // turn box red if the bounding box is withing a certain distance of the center of the feed
+  if (previous_box.width > 0)
+  {
+    if (center_pt.x - previous_box.x < distance)
+    {
+      // turn the box red if the bounding box is inside the distance threshold
+      cv::rectangle(cropped_frame, previous_box, cv::Scalar(0, 0, 255), 2);
+    }
+    else
+    {
+      // turn the box green if the bounding box is outside the distance threshold
+      cv::rectangle(cropped_frame, previous_box, cv::Scalar(0, 255, 0), 2);
+    }
+  }
+  return previous_box;
+}
+
+cv::Rect getYellowBoundingBox(cv::Mat cropped_frame, float distance)
+{
+  // bgr (blue, green, red)
+  cv::Scalar yellow(0, 255, 255);
+  cv::Rect previous_box(cv::Point(0, 0), cv::Size(0, 0));
+  // create a bounding box from the contour points of the cone and draw it
+  // only if the size of the bounding box is larger than a certain threshold
+  for (auto &contour : y_pts)
+  {
+    cv::Rect bounding_box = cv::boundingRect(contour);
+    if (bounding_box.area() > threshold)
+    {
+      cv::Rect box = cv::boundingRect(contour);
+      // draw the bounding box
+      cv::rectangle(cropped_frame, box, yellow, 2);
+      // draw the center of the bounding box
+      cv::Point center(box.x + box.width / 2, box.y + box.height / 2);
+      cv::circle(cropped_frame, center, 5, yellow, -1);
+      // draw the center of the previous bounding box
+      if (previous_box.width > 0)
+      {
+        cv::Point previous_center(previous_box.x + previous_box.width / 2, previous_box.y + previous_box.height / 2);
+        cv::circle(cropped_frame, previous_center, 5, yellow, -1);
+        // draw a line between the center of the previous bounding box and the center of the current bounding box
+        cv::line(cropped_frame, previous_center, center, yellow, 2);
+      }
+      previous_box = box;
+    }
+  }
+
+  // turn box red if the bounding box is withing a certain distance of the center of the feed
+  if (previous_box.width > 0)
+  {
+    if (center_pt.x - previous_box.x < distance)
+    {
+      // turn the box red if the bounding box is inside the distance threshold
+      cv::rectangle(cropped_frame, previous_box, cv::Scalar(0, 0, 255), 2);
+    }
+    else
+    {
+      // turn the box green if the bounding box is outside the distance threshold
+      cv::rectangle(cropped_frame, previous_box, cv::Scalar(0, 255, 0), 2);
+    }
+  }
+  return previous_box;
+}
+
+// calculate the give steering adjustment based on the
+// distance of the blue cones bounding box from the center of the feed
+// distance of the yellow cones bounding box from the center of the feed
+float getSteeringAngle(float distance)
+{
+  // segment blue
+  cv::inRange(Imgs.img_hsv, blue.low, blue.high, Imgs.fr_hsv);
+  b_detected = detectBlueCones(Imgs.fr_hsv);
+  cv::Rect b_bounding_box(getBlueBoundingBox(Imgs.fr_cropped, distance));
+  // segment yellow
+  cv::inRange(Imgs.img_hsv, yellow.low, yellow.high, Imgs.fr_hsv);
+  y_detected = detectYellowCones(Imgs.fr_hsv);
+  cv::Rect y_bounding_box(getYellowBoundingBox(Imgs.fr_cropped, distance));
+  // get the current steering angle based on the bounding boxes
+
+  // display the current angle on the feed
+  cv::putText(Imgs.fr_cropped, std::to_string(b_mag), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
+
+  // draw the center point as a green colored circle when distance is greater than the threshold
+  // and a red colored circle when distance is less than the threshold
+  if (distance > threshold)
+  {
+    cv::circle(Imgs.fr_cropped, center_pt, 5, cv::Scalar(0, 255, 0), -1);
+  }
+  else
+  {
+    cv::circle(Imgs.fr_cropped, center_pt, 5, cv::Scalar(0, 0, 255), -1);
+  }
+  // TODO currently not complete so return 0.0
+  return 0.0;
 }
 
 int32_t main(int32_t argc, char **argv)
@@ -160,54 +271,22 @@ int32_t main(int32_t argc, char **argv)
       // Endless loop; end the program by pressing Ctrl-C.
       while (od4.isRunning())
       {
-        std::stringstream ss;  // stringstream to hold text for the image.
+        std::stringstream ss; // stringstream to hold text for the image.
         // std::string timestamp; // timestamp :D
         std::pair<bool, cluon::data::TimeStamp> ts;
 
         // Wait for a new frame.
         sharedMemory->wait();
-        uint64_t start_f = cv::getTickCount();
+        // uint64_t start_f = cv::getTickCount();
 
         sharedMemory->lock();
         {
           ts = sharedMemory->getTimeStamp();
           cv::Mat wrapped(HEIGHT, WIDTH, CV_8UC4, sharedMemory->data());
-          Imgs.main = wrapped.clone();  // text
+          Imgs.main = wrapped.clone(); // text
           // timestamp = get_timestamp(sharedMemory->getTimeStamp(), (cluon::time::now()).seconds());
         }
         sharedMemory->unlock();
-
-        [&ss, &dr, &gsr](){
-          ss << "Blue Detected:   " << (b_detected ? "true" : "false");
-          cv::putText(Imgs.main, ss.str(), cv::Point(10, 377), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
-          ss.str("");
-          ss.clear();
-
-          ss << "Yellow Detected:   " << (y_detected ? "true" : "false");
-          cv::putText(Imgs.main, ss.str(), cv::Point(10, 393), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
-          ss.str("");
-          ss.clear();
-
-          ss << "ActualDistanceReading:   " << dr.distance();
-          cv::putText(Imgs.main, ss.str(), cv::Point(10, 409), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
-          ss.str("");
-          ss.clear();
-
-          ss << "ComputedDistanceReading: " << dr.distance();
-          cv::putText(Imgs.main, ss.str(), cv::Point(10, 425), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
-          ss.str("");
-          ss.clear();
-
-          ss << "ActualSteering:   " << gsr.groundSteering();
-          cv::putText(Imgs.main, ss.str(), cv::Point(10, 441), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
-          ss.str("");
-          ss.clear();
-
-          ss << "ComputedSteering: " << 0.00000;
-          cv::putText(Imgs.main, ss.str(), cv::Point(10, 457), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
-          ss.str("");
-          ss.clear();
-        }();
 
         // Cropped image frame (only ned a small slice the rest is noice)
         Imgs.fr_cropped = Imgs.main(roi);
@@ -215,16 +294,39 @@ int32_t main(int32_t argc, char **argv)
         cv::blur(Imgs.fr_cropped, Imgs.img_blur, cv::Size(7, 7));
         // convert bgr to hsv
         cv::cvtColor(Imgs.img_blur, Imgs.img_hsv, cv::COLOR_BGR2HSV);
-        // segment blue
-        cv::inRange(Imgs.img_hsv, blue.low, blue.high, Imgs.fr_hsv);
-        b_detected = detect_blue(Imgs.fr_hsv);
-        // segment yellow
-        cv::inRange(Imgs.img_hsv, yellow.low, yellow.high, Imgs.fr_hsv);
-        y_detected = detect_yellow(Imgs.fr_hsv);
+        // calculate steering adjustment based on the distance from the center of the image
+        // and the yellow and blue bounding boxes
+        steering_angle = getSteeringAngle(dr.distance());
+
+        ss << "Blue Detected:   " << (b_detected ? "true" : "false");
+        cv::putText(Imgs.main, ss.str(), cv::Point(10, 377), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
+        ss.str("");
+        ss.clear();
+
+        ss << "Yellow Detected:   " << (y_detected ? "true" : "false");
+        cv::putText(Imgs.main, ss.str(), cv::Point(10, 393), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
+        ss.str("");
+        ss.clear();
+
+        ss << "ActualDistanceReading:   " << dr.distance();
+        cv::putText(Imgs.main, ss.str(), cv::Point(10, 409), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
+        ss.str("");
+        ss.clear();
+
+        ss << "ActualSteering:   " << gsr.groundSteering();
+        cv::putText(Imgs.main, ss.str(), cv::Point(10, 425), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
+        ss.str("");
+        ss.clear();
+
+        ss << "ComputedSteering: " << steering_angle;
+        cv::putText(Imgs.main, ss.str(), cv::Point(10, 441), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 255, 255), 1);
+        ss.str("");
+        ss.clear();
 
         // get the ending frame tick count -> performance calculation
-        uint64_t end_f = cv::getTickCount();
-        turn_in_timestamp(ts, 0.0000);
+        // uint64_t end_f = cv::getTickCount();
+        // the turn in time stamp requirement
+        turn_in_timestamp(ts, steering_angle);
         if (VERBOSE)
         {
           Imgs.hsv_debug = Imgs.img_hsv.clone();
