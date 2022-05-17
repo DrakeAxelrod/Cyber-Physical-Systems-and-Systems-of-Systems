17453745ada7be672368e9c5b53f316a1c463bc4
Drake Axelrod <drake@draxel.io>
2022-05-17 14:16:10 +0200

    Add sleep to py script
    so curl has time to download

--- scripts/parse-prev-job-id-and-download.py
+++ scripts/parse-prev-job-id-and-download.py
@@ -6,17 +6,20 @@ import json
 import subprocess
 import zipfile
 import os
+import time
 
 TOKEN = os.environ["ACCESS_TOKEN"]
 API = "https://git.chalmers.se/api/v4/projects/5345"
 
 if __name__ == "__main__":
-    url = f"{API}/jobs"
-    response = requests.get(url, headers={ "PRIVATE-TOKEN": TOKEN })
-    data = json.loads(response.text)
-    id = data[1]["id"]
-    subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"PRIVATE-TOKEN: {TOKEN}", f"{API}/jobs/{id}/artifacts"])
-    with zipfile.ZipFile("vresults.zip", "r") as zip_ref:
-        zip_ref.extractall(".")
-    subprocess.run(["rm", "vresults.zip"])
-    subprocess.run(["mv", "vresults", "verification-results"])
+  url = f"{API}/jobs"
+  response = requests.get(url, headers={ "PRIVATE-TOKEN": TOKEN })
+  data = json.loads(response.text)
+  id = data[1]["id"]
+  subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"PRIVATE-TOKEN: {TOKEN}", f"{API}/jobs/{id}/artifacts"])
+  # zzz
+  sleep(5)
+  with zipfile.ZipFile("vresults.zip", "r") as zip_ref:
+      zip_ref.extractall(".")
+  subprocess.run(["rm", "vresults.zip"])
+  subprocess.run(["mv", "vresults", "verification-results"])
