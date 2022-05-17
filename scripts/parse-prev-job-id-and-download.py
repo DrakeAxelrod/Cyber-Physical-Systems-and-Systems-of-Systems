acfee84f1c314169fb7ccf66a78da7fdaf0f579f
Drake Axelrod <drake@draxel.io>
2022-05-17 14:22:14 +0200

    Add token as arg via pipeline

--- scripts/parse-prev-job-id-and-download.py
+++ scripts/parse-prev-job-id-and-download.py
@@ -7,18 +7,17 @@ import subprocess
 import zipfile
 import os
 import time
+import sys
 
-TOKEN = os.environ["ACCESS_TOKEN"]
-API = "https://git.chalmers.se/api/v4/projects/5345"
 
 if __name__ == "__main__":
+  TOKEN = sys.argv[1]
+  API = "https://git.chalmers.se/api/v4/projects/5345"
   url = f"{API}/jobs"
   response = requests.get(url, headers={ "PRIVATE-TOKEN": TOKEN })
   data = json.loads(response.text)
   id = data[1]["id"]
   subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"PRIVATE-TOKEN: {TOKEN}", f"{API}/jobs/{id}/artifacts"])
-  # zzz
-  time.sleep(5)
   with zipfile.ZipFile("vresults.zip", "r") as zip_ref:
       zip_ref.extractall(".")
   subprocess.run(["rm", "vresults.zip"])
