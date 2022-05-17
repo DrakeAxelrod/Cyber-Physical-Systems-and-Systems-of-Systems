3506e5483533ba98162584b49fee24e765db39ca
Drake Axelrod <drake@draxel.io>
2022-05-17 14:26:55 +0200

    Check filesystem of runner

--- scripts/parse-prev-job-id-and-download.py
+++ scripts/parse-prev-job-id-and-download.py
@@ -9,15 +9,18 @@ import os
 import time
 import sys
 
+TOKEN = os.environ["ACCESS_TOKEN"]
+
 
 if __name__ == "__main__":
-  TOKEN = "glpat-UAyNtbYBMZQxPv_h2jzs"
+  TOKEN = os.environ["ACCESS_TOKEN"]
   API = "https://git.chalmers.se/api/v4/projects/5345"
   url = f"{API}/jobs"
   response = requests.get(url, headers={ "PRIVATE-TOKEN": TOKEN })
   data = json.loads(response.text)
   id = data[1]["id"]
   subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"PRIVATE-TOKEN: {TOKEN}", f"{API}/jobs/{id}/artifacts"])
+  subprocess.run(["ls", "-al"])
   with zipfile.ZipFile("vresults.zip", "r") as zip_ref:
       zip_ref.extractall(".")
   subprocess.run(["rm", "vresults.zip"])
