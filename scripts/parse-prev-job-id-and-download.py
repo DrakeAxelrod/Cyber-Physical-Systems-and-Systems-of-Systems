e8f34ff0ec1bc75cd7bb9246b4179e3c782c050f
Drake Axelrod <drake@draxel.io>
2022-05-17 17:20:59 +0200

    Try without python script

--- scripts/parse-prev-job-id-and-download.py
+++ scripts/parse-prev-job-id-and-download.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
   data = json.loads(response.text)
   id = data[1]["id"]
   # curl --output test.zip -H "PRIVATE-TOKEN: glpat-UAyNtbYBMZQxPv_h2jzs" https://git.chalmers.se/api/v4/projects/5345/jobs/197615/artifacts
+  # "curl -location --output vresults.zip https://git.chalmers.se/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/main/download?job=build-and-test&job_token=$CI_JOB_TOKEN"
   subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"JOB-TOKEN: {sys.argv[1]}", f"{API}/jobs/{id}/artifacts"])
   subprocess.run(["ls", "-al"])
   subprocess.run(["cat", "vresults.zip"])
