c3041fecd91a30282a27eac331e789ddc24a20fe
Drake Axelrod <drake@draxel.io>
2022-05-17 17:31:15 +0200

    Try to ls with python

--- scripts/parse-prev-job-id-and-download.py
+++ scripts/parse-prev-job-id-and-download.py
@@ -10,17 +10,17 @@ import time
 import sys
 
 if __name__ == "__main__":
-  TOKEN = "glpat-UAyNtbYBMZQxPv_h2jzs" #sys.argv[1]
-  API = "https://git.chalmers.se/api/v4/projects/5345"
-  url = f"{API}/jobs"
-  response = requests.get(url, headers={ "PRIVATE-TOKEN": TOKEN })
-  data = json.loads(response.text)
-  id = data[1]["id"]
+  # TOKEN = "glpat-UAyNtbYBMZQxPv_h2jzs" #sys.argv[1]
+  # API = "https://git.chalmers.se/api/v4/projects/5345"
+  # url = f"{API}/jobs"
+  # response = requests.get(url, headers={ "PRIVATE-TOKEN": TOKEN })
+  # data = json.loads(response.text)
+  # id = data[1]["id"]
   # curl --output test.zip -H "PRIVATE-TOKEN: glpat-UAyNtbYBMZQxPv_h2jzs" https://git.chalmers.se/api/v4/projects/5345/jobs/197615/artifacts
   # "curl -location --output vresults.zip https://git.chalmers.se/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/main/download?job=build-and-test&job_token=$CI_JOB_TOKEN"
-  subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"JOB-TOKEN: {sys.argv[1]}", f"{API}/jobs/{id}/artifacts"])
+  # subprocess.run(["curl", "-o", "vresults.zip", "--globoff", "-H", f"JOB-TOKEN: {sys.argv[1]}", f"{API}/jobs/{id}/artifacts"])
   subprocess.run(["ls", "-al"])
-  subprocess.run(["cat", "vresults.zip"])
-  with zipfile.ZipFile("./vresults.zip", "r") as zip_ref:
-      zip_ref.extractall(".")
-  subprocess.run(["mv", "vresults", "verification-results"])
+  # subprocess.run(["cat", "vresults.zip"])
+  # with zipfile.ZipFile("./vresults.zip", "r") as zip_ref:
+  #     zip_ref.extractall(".")
+  # subprocess.run(["mv", "vresults", "verification-results"])
