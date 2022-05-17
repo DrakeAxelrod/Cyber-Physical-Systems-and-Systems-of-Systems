9f7f95836314411865354e634e9cfefc4165b31a
Drake Axelrod <drake@draxel.io>
2022-05-17 13:45:25 +0200

    Add python/curl magic to get previous artifact

---
+++ scripts/get-artifact.sh
@@ -0,0 +1,7 @@
+# curl --globoff --header "PRIVATE-TOKEN: glpat-UAyNtbYBMZQxPv_h2jzs" "https://git.chalmers.se/courses/dit638/students/2022-group-05/-/jobs/197615/artifacts/download"
+# curl --globoff --header "PRIVATE-TOKEN: glpat-UAyNtbYBMZQxPv_h2jzs" "https://git.chalmers.se/api/v4/projects/courses/dit638/students/2022-group-05/jobs/artifacts/main"
+# https://git.chalmers.se/courses/dit638/students/2022-group-05/-/jobs/197615/artifacts/download
+# https://gitlab.com/api/v4/projects/awesome-organization%2Fawesome-group%2Fowesome-project/jobs/artifacts/master/download?job=publish-release
+# https://git.chalmers.se/api/v4/projects/courses/dit638/students/2022-group-05/
+
+
