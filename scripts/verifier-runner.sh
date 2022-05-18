96c0047aad83975cb3e0f64e5103e6047c0b1046
Drake Axelrod <drake@draxel.io>
2022-05-18 19:26:49 +0200

    Down down the rabbit hole we go
    I dont know what is up and I really don't know what id down...
    but atleast the flowers are in bloom.

--- scripts/verifier-runner.sh
+++ scripts/verifier-runner.sh
@@ -4,4 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 opendlv-video-h264-replay --cid="20$3" --name="img$3" "$1" &
 sleep 0.5
-solution --cid="20$3" --name="img$3" --width=640 --height=480 --output="/opt/sources/verification-results/csvs/${2%.*}.csv" &
+solution --cid="20$3" --name="img$3" --width=640 --height=480 --output="/opt/sources/verification-results/csvs/${2%.*}.csv"
+
+unset SOLUTION_PID
